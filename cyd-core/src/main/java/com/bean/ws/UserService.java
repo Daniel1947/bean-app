4c993965927df1df64c96687de691856086bfa62
Daniel <sei.dengzun@gmail.com>
2014-08-20 15:24:05 +0800

    load table

--- cyd-core/src/main/java/com/bean/ws/UserService.java
+++ cyd-core/src/main/java/com/bean/ws/UserService.java
@@ -10,7 +10,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Daniel on 14-8-18.
@@ -24,6 +26,8 @@ public class UserService {
     public Response getAllStores() throws Exception{
 
         List<UserVO> allUser = UserProxy.getAllUsers();
-        return ServiceUtil.buildResponse(allUser);
+        Map<String, List> result = new HashMap<String, List>();
+        result.put("users", allUser);
+        return ServiceUtil.buildResponse(result);
     }
 }
